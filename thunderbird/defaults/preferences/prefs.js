68ae0081ade4680ff32a41dc527b0c502b9e7334
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-02-23 11:36:11 +0100

    Set prefs 2

--- thunderbird/defaults/preferences/prefs.js
+++ thunderbird/defaults/preferences/prefs.js
@@ -1,4 +1,4 @@
-pref("extensions.kgen@elitwork.com.description","chrome://kgen/locale/kgen.properties");
+pref("extensions.kgen@elitwork.com.description","chrome://capskiller/locale/capskiller.properties");
 pref("extensions.kgen@elitwork.com.maxcaps",1);
 pref("extensions.kgen@elitwork.com.filtersubject",true);
 pref("extensions.kgen@elitwork.com.filtercontent",true);
